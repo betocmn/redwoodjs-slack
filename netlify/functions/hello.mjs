1d59f7bea72adea3a3eb8c194477b21310d6ba6a
en0c-026 <bitup.games@gmail.com>
2023-11-06 20:21:25 +0100

    update

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -2,6 +2,7 @@ import { Redis } from "@upstash/redis";
 
 const redis = Redis.fromEnv();
 export default async () => {
+  console.log(process.env.SLACK_BOT_TOKEN);
   const counter = await redis.incr("edge_counter");
   return new Response(counter);
 };
