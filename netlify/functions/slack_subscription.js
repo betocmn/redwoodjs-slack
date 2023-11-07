82984fa6bee7d7dc055845841986366a618ce2d9
en0c-026 <bitup.games@gmail.com>
2023-11-07 14:21:43 +0100

    update

--- netlify/functions/slack_subscription.js
+++ netlify/functions/slack_subscription.js
@@ -101,63 +101,63 @@ const logDesicionView = {
   },
 }
 
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateVerification: false,
+  scopes: ['chat:write', 'channels:history', 'commands', 'channels:read'],
+  installationStore,
+  processBeforeResponse: true,
+
+})
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  authorize: authorizeFn,
+  receiver,
+  logLevel: LogLevel.DEBUG
+});
+
+
+app.shortcut('log_decision', async ({ shortcut, ack, client, logger }) => {
+  try {
+    // Acknowledge shortcut request
+    await ack();
+
+    // Call the views.open method using one of the built-in WebClients
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: logDesicionView,
+    })
+
+    logger.info(result);
+  }
+  catch (error) {
+    logger.error(error);
+  }
+});
+
+app.view('log_decision', async ({ body, ack, say, logger }) => {
+  await ack();
+  logger.info('from view listener', JSON.stringify(body.view.state, null, 2));
+});
+
+app.message('hi', async ({ message, say, logger }) => {
+  logger.info('message received: ', message.text)
+  // say() sends a message to the channel where the event was triggered
+  try {
+    await say(`Hey there <@${message.user}>!`);
+
+  } catch (error) {
+    logger.error(error)
+  }
+});
 
 module.exports.handler = async (req, context) => {
-  const receiver = new ExpressReceiver({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    clientId: process.env.SLACK_CLIENT_ID,
-    clientSecret: process.env.SLACK_CLIENT_SECRET,
-    stateVerification: false,
-    scopes: ['chat:write', 'channels:history', 'commands', 'channels:read'],
-    installationStore,
-    processBeforeResponse: true,
-
-  })
-
-  const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    authorize: authorizeFn,
-    receiver,
-    logLevel: LogLevel.DEBUG
-  });
-
-
-  app.shortcut('log_decision', async ({ shortcut, ack, client, logger }) => {
-    try {
-      // Acknowledge shortcut request
-      await ack();
-
-      // Call the views.open method using one of the built-in WebClients
-      const result = await client.views.open({
-        trigger_id: shortcut.trigger_id,
-        view: logDesicionView,
-      })
-
-      logger.info(result);
-    }
-    catch (error) {
-      logger.error(error);
-    }
-  });
-
-  app.view('log_decision', async ({ body, ack, say, logger }) => {
-    await ack();
-    logger.info('from view listener', JSON.stringify(body.view.state, null, 2));
-  });
-
-  app.message('hi', async ({ message, say, logger }) => {
-    logger.info('message received: ', message.text)
-    // say() sends a message to the channel where the event was triggered
-    try {
-      await say(`Hey there <@${message.user}>!`);
-
-    } catch (error) {
-      logger.error(error)
-    }
-  });
 
   await app.start()
-  return await app.receiver.requestHandler(req, 'ok')
+  return await receiver.requestHandler(req, 'ok')
 }
 // module.exports.handler = async (req, context) => {
 //   const payload = parseRequestBody(req.body, req.headers["content-type"]);
