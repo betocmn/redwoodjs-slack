2fabc1ec787e6980a6f873d943955d1b2479f2fa
en0c-026 <bitup.games@gmail.com>
2023-11-07 12:16:58 +0100

    update

--- netlify/functions/slack_subscription.js
+++ netlify/functions/slack_subscription.js
@@ -153,7 +153,7 @@ app.message('hi', async ({ message, say, logger }) => {
 });
 
 
-const handler = serverless(receiver.app);
+const handler = serverless(app.receiver.app);
 
 module.exports.handler = async (req, context) => {
   return await handler(req, context);
