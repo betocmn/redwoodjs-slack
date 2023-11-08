1e9fe40579af2d66f5d59590832733f41e1558fd
Beto de Castro Moreira <humberto.mn@gmail.com>
2023-11-08 20:57:44 +1000

    Convert slack install to JS

---
+++ api/src/functions/slack_install.js
@@ -0,0 +1,11 @@
+exports.handler = async (event, context) => {
+  const clientId = process.env.SLACK_CLIENT_ID;
+  const scopes = ['chat:write', 'channels:history', 'commands', 'channels:read'];
+
+  return {
+    statusCode: 301,
+    headers: {
+      'Location': `https://slack.com/oauth/v2/authorize?scope=${scopes.join(',')}&client_id=${clientId}`
+    }
+  };
+};
--- api/src/functions/slack_install.mjs
+++
@@ -1,7 +0,0 @@
-
-export default async (req, context) => {
-  const clientId = process.env.SLACK_CLIENT_ID;
-
-  const scopes = ['chat:write', 'channels:history', 'commands', 'channels:read']
-  return Response.redirect(`https://slack.com/oauth/v2/authorize?scope=${scopes.join(',')}&client_id=${clientId}`, 301);
-};
