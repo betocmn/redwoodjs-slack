f740db13bb434e7f61aa16153e700f355224cc6d
Beto de Castro Moreira <humberto.mn@gmail.com>
2023-11-08 20:43:25 +1000

    Update slack install

--- api/functions/slack_install.js
+++ api/functions/slack_install.js
@@ -1,7 +1,6 @@
 
 export default async (req, context) => {
-  const clientId = Netlify.env.get("SLACK_CLIENT_ID");
-  const redirectUri = Netlify.env.get("INSTALL_REDIRECT_URI");
+  const clientId = process.env.SLACK_CLIENT_ID;
 
   const scopes = ['chat:write', 'channels:history', 'commands', 'channels:read']
   return Response.redirect(`https://slack.com/oauth/v2/authorize?scope=${scopes.join(',')}&client_id=${clientId}`, 301);
