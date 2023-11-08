fba07025ad04cf6ffd0ff31d764cc7cf2ae3fda3
en0c-026 <bitup.games@gmail.com>
2023-11-08 06:15:12 +0100

    update

--- netlify/functions/slack_subscription.mjs
+++ netlify/functions/slack_subscription.mjs
@@ -2,46 +2,44 @@ import { App, ExpressReceiver, LogLevel } from '@slack/bolt';
 import { Redis } from "@upstash/redis";
 import serverless from 'serverless-http';
 
+// Initialize Redis connection
 const redis = Redis.fromEnv();
 
+// Define an installation store for Slack app installations
 const installationStore = {
+  // Check if it's an Enterprise Install or a single Team App Install and save accordingly
   storeInstallation: async (installation) => {
-    // change the line below so it saves to your database
     if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
-      // support for org wide app installation
       return await redis.set(installation.enterprise.id, installation);
     }
     if (installation.team !== undefined) {
-      // single team app installation
       return await redis.set(installation.team.id, installation);
     }
     throw new Error('Failed saving installation data to installationStore');
   },
+  // Function to fetch installation data
   fetchInstallation: async (installQuery) => {
-    // change the line below so it fetches from your database
+
     if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-      // org wide app installation lookup
       return await redis.get(installQuery.enterpriseId);
     }
     if (installQuery.teamId !== undefined) {
-      // single team app installation lookup
       return await redis.get(installQuery.teamId);
     }
     throw new Error('Failed fetching installation');
   },
+  // Function to delete installation data
   deleteInstallation: async (installQuery) => {
-    // change the line below so it deletes from your database
     if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-      // org wide app installation deletion
       return await redis.del(installQuery.enterpriseId);
     }
     if (installQuery.teamId !== undefined) {
-      // single team app installation deletion
       return await redis.del(installQuery.teamId);
     }
     throw new Error('Failed to delete installation');
   },
 }
+// Define an authorization function
 const authorizeFn = async ({ teamId, enterpriseId }) => {
   if (enterpriseId) {
     const installation = await redis.get(enterpriseId)
@@ -67,6 +65,45 @@ const authorizeFn = async ({ teamId, enterpriseId }) => {
 
 
 }
+
+// Initialize the Slack Bolt ExpressReceiver
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateVerification: false,
+  scopes: ['chat:write', 'channels:history', 'commands', 'channels:read'],
+  installationStore,
+  processBeforeResponse: true,
+})
+
+// Create a Slack Bolt app
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  authorize: authorizeFn,
+  receiver,
+  logLevel: LogLevel.DEBUG
+});
+
+app.shortcut('log_decision', async ({ shortcut, ack, client, logger }) => {
+  try {
+    // Acknowledge shortcut request
+    await ack();
+
+    // Call the views.open method using one of the built-in WebClients
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: logDesicionView,
+    })
+
+    logger.info(result);
+  }
+  catch (error) {
+    logger.error(error);
+  }
+});
+
+// Define a modal view for logging decisions
 const logDesicionView = {
   type: 'modal',
   callback_id: 'log_decision', // Add this line
@@ -95,42 +132,6 @@ const logDesicionView = {
   },
 }
 
-const receiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  clientId: process.env.SLACK_CLIENT_ID,
-  clientSecret: process.env.SLACK_CLIENT_SECRET,
-  stateVerification: false,
-  scopes: ['chat:write', 'channels:history', 'commands', 'channels:read'],
-  installationStore,
-  processBeforeResponse: true,
-})
-
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  authorize: authorizeFn,
-  receiver,
-  logLevel: LogLevel.DEBUG
-});
-
-
-app.shortcut('log_decision', async ({ shortcut, ack, client, logger }) => {
-  try {
-    // Acknowledge shortcut request
-    await ack();
-
-    // Call the views.open method using one of the built-in WebClients
-    const result = await client.views.open({
-      trigger_id: shortcut.trigger_id,
-      view: logDesicionView,
-    })
-
-    logger.info(result);
-  }
-  catch (error) {
-    logger.error(error);
-  }
-});
-
 app.view('log_decision', async ({ body, ack, say, logger }) => {
   await ack();
   logger.info('from view listener', JSON.stringify(body.view.state, null, 2));
@@ -144,6 +145,7 @@ app.message('hi', async ({ message, say, logger }) => {
   }
 });
 
+// Export a serverless handler for the app
 export const handler = serverless(receiver.app)
 
 
