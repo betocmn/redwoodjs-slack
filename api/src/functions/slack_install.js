74258e1c9ba8be5fe996630533cb80b3bc381b0d
Beto de Castro Moreira <humberto.mn@gmail.com>
2023-11-08 21:50:49 +1000

    Update slack install with bolt

--- api/src/functions/slack_install.js
+++ api/src/functions/slack_install.js
@@ -1,4 +1,4 @@
-export const handler = async (event, context) => {
+/*export const handler = async (event, context) => {
   const clientId = process.env.SLACK_CLIENT_ID;
   const scopes = ['chat:write', 'channels:history', 'commands', 'channels:read'];
 
@@ -8,4 +8,37 @@ export const handler = async (event, context) => {
       'Location': `https://slack.com/oauth/v2/authorize?scope=${scopes.join(',')}&client_id=${clientId}`
     },
   };
-};
+};*/
+
+import serverlessHttp from 'serverless-http'
+
+import { installExpressReceiver } from 'src/lib/slack'
+
+export const handler = serverlessHttp(async (req, res) => {
+  try {
+    const url = await installExpressReceiver.installer.generateInstallUrl({
+      scopes: [
+        'chat:write',
+        'channels:history',
+        'commands',
+        'channels:read',
+        /*
+        'chat:write',
+        'commands',
+        'users.profile:read',
+        'users:read',
+        'users:read.email',
+        */
+      ],
+      userScopes: ['email', 'profile', 'openid'],
+    })
+
+    res.writeHead(302, { Location: url })
+    res.end()
+  } catch (error) {
+    console.error(error)
+    res.writeHead(500)
+    res.end('Failed to create install url')
+  }
+})
+
---
+++ api/src/lib/slack.ts
@@ -0,0 +1,91 @@
+import { ExpressReceiver, LogLevel } from '@slack/bolt'
+import { WebClient } from '@slack/web-api'
+import { Redis } from "@upstash/redis";
+
+// Initialize Redis connection
+const redis = Redis.fromEnv();
+
+// Define an installation store for Slack app installations
+const installationStore = {
+  // Check if it's an Enterprise Install or a single Team App Install and save accordingly
+  storeInstallation: async (installation) => {
+    if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
+      return await redis.set(installation.enterprise.id, installation);
+    }
+    if (installation.team !== undefined) {
+      return await redis.set(installation.team.id, installation);
+    }
+    throw new Error('Failed saving installation data to installationStore');
+  },
+  // Function to fetch installation data
+  fetchInstallation: async (installQuery) => {
+
+    if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+      return await redis.get(installQuery.enterpriseId);
+    }
+    if (installQuery.teamId !== undefined) {
+      return await redis.get(installQuery.teamId);
+    }
+    throw new Error('Failed fetching installation');
+  },
+  // Function to delete installation data
+  deleteInstallation: async (installQuery) => {
+    if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+      return await redis.del(installQuery.enterpriseId);
+    }
+    if (installQuery.teamId !== undefined) {
+      return await redis.del(installQuery.teamId);
+    }
+    throw new Error('Failed to delete installation');
+  },
+}
+
+// Define an authorization function
+const authorizeFn = async ({ teamId, enterpriseId }) => {
+  if (enterpriseId) {
+    const installation = await redis.get(enterpriseId)
+    return {
+      botToken: installation.access_token,
+      botUserId: installation.bot_user_id,
+      userId: installation.authed_user.id,
+      teamId: installation.team?.id,
+      enterpriseId: installation.enterprise?.id,
+    };
+  }
+  if (teamId) {
+    const installation = await redis.get(teamId)
+    return {
+      botToken: installation.access_token,
+      botUserId: installation.bot_user_id,
+      userId: installation.authed_user.id,
+      teamId: installation.team?.id,
+      enterpriseId: installation.enterprise?.id,
+    };
+  }
+  throw new Error('No matching authorizations');
+
+
+}
+
+export const installExpressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  processBeforeResponse: true, // required to be true for OAuth
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: 'test',
+  logLevel: LogLevel.DEBUG,
+  scopes: [
+    'chat:write',
+    'channels:history',
+    'commands',
+    'channels:read',
+  ],
+  redirectUri:
+    'https://spiffy-sunburst-210bfc.netlify.app/.netlify/functions/oauth_redirect',
+  installerOptions: {
+    redirectUriPath: '/.netlify/functions/oauth_redirect',
+    userScopes: ['email', 'profile', 'openid'],
+    legacyStateVerification: true,
+  },
+  installationStore: installationStore,
+})
